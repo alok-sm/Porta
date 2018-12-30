deffb26a389d2a2cbd7faeb1b7ce80526fec1f91
Alok Mysore <alok.shankar.m@gmail.com>
2018-12-30 12:37:41 -0800

    latest

--- Viewer/viewer.py
+++ Viewer/viewer.py
@@ -17,11 +17,12 @@ def view(args):
     # os.system('source ~/.bashrc; tab ~/Desktop/ http-server')
     
     
-    # run_in_new_tab('/Applications/Chromium.app/Contents/MacOS/Chromium http://localhost:9000 --disable-web-security --user-data-dir --allow-running-insecure-content --profile-directory=\\\'Profile 1\\\'')
+    run_in_new_tab('/Applications/Chromium.app/Contents/MacOS/Chromium http://localhost:9000 --disable-web-security --user-data-dir --allow-running-insecure-content --profile-directory=\\\'Profile 1\\\'')
     
-    run_in_new_tab('/Applications/Google\\\\\\\\ Chrome.app/Contents/MacOS/Google\\\\\\\\ Chrome --disable-web-security --user-data-dir --allow-running-insecure-content')
+    # run_in_new_tab('/Applications/Google\\\\\\\\ Chrome.app/Contents/MacOS/Google\\\\\\\\ Chrome --disable-web-security --user-data-dir --allow-running-insecure-content')
     run_in_new_tab('grunt serve', '~/dev/HciResearch/Porta/Viewer/Frontend/')
-    run_in_new_tab('http-server', '~/Google\ Drive\ \(amysore\@eng.ucsd.edu\)/porta-userstudy/LogFiles/_screencasts/')
+    # run_in_new_tab('http-server', '~/Google\ Drive\ \(amysore\@eng.ucsd.edu\)/porta-userstudy/LogFiles/_screencasts/')
+    run_in_new_tab('http-server', '/Users/alok/Movies')
 
     view_server = ViewServer()
     view_server.start()
